a223bc4a0dc96e0e745b90d2d93a43330620dee0
kkolyszko <katarzyna.kolyszko@gmail.com>
2017-10-21 18:45:58 +0200

    dodane skrypty do przyciskówv1

---
+++ script.js
@@ -0,0 +1,37 @@
+/*var person = {
+	name: "Jan",
+	sayHello: function() {
+		console.log("Hello "  + this.name + "!");
+    }
+}
+var hello = person.sayHello;
+
+hello();*/
+
+
+
+function Button(text) {
+    this.text = text || 'Hello';
+}
+
+Button.prototype = {
+    create: function() {
+        var self = this;
+        this.$element = $('<br><button>');
+        this.$element.text(this.text);
+        this.$element.click(function() {
+            alert(self.text);
+        });
+        $('.content').append(this.$element);
+                    
+    }
+}
+
+//var btn1 = new Button("What's up!");
+//btn1.create();
+
+$('#mainBtn').click(function(){
+    btnText = prompt('Enter text');
+    var btn1 = new Button(btnText);
+        btn1.create();
+});
